bd392f1ccd7db2f8017f51d53d24e0b79faa5184
Yogesh Jagdale <yog99mail@gmail.com>
2020-04-22 20:28:08 +0530

    set default port, added .gitignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT_NUMBER
+const PORT = process.env.PORT_NUMBER || 8000
 const SERVER = require('./server')
 SERVER(PORT)
