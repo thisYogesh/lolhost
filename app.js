2a944910fea6d6406689c41059717ce198c168fc
Yogesh Jagdale <yog99mail@gmail.com>
2020-05-16 19:29:29 +0530

    fs update

--- app.js
+++
@@ -1,2 +0,0 @@
-require('./index')(8000)
-require('./api')(1000)
--- js/cli.js
+++ js/cli.js
@@ -2,4 +2,4 @@
 
 const [,, ...argv] = process.argv
 const port = argv[0] === '--port' ? argv[1] : 8000
-require('./index')(port)
+require('../index')(port)
